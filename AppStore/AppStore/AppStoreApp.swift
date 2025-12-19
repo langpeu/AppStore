bed8c1d81ba4c916f32f3032823f042a25240b86
langpeu <icopy@naver.com>
2025-12-20 00:21:25 +0900

    init

---
+++ AppStore/AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by Langpeu on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStore/AppStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppStore
+//
+//  Created by Langpeu on 12/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
