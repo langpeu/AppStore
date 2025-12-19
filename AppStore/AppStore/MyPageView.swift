77292f532f458615ee45b8e3637ecf2ee790a304
langpeu <icopy@naver.com>
2025-12-20 00:58:28 +0900

    S2-6,7,8 6. SwiftUI로 마이페이지 목록 UI 구성하기 7. SwiftData 기본 사용법 알아보기 CRUD 8. SwiftData 세팅해서 쿼리 날려보기

--- AppStore/AppStore/MyPageView.swift
+++ AppStore/AppStore/MyPageView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 import Alamofire
 import ComposableArchitecture
 
+// 이름변경,이메일변경, 이미지변경
+enum MyPagePath: CaseIterable {
+    case name
+    case email
+    case image
+    
+    var title: String {
+        switch self {
+        case .name:
+            "이름"
+        case .email:
+            "이메일"
+        case .image:
+            "프로필 이미지"
+        }
+    }
+}
+
 
 struct MyPageView: View {
     var body: some View {
-        VStack {
-       
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                ForEach(MyPagePath.allCases, id: \.self) { option in
+                    listItem(option: option)
+                }
+            }
         }
-        .padding()
+    }
+    
+    func listItem(option: MyPagePath) -> some View {
+        Button {
+            //TODO: 버튼 클릭 액션
+        } label: {
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(option.title)
+                        .foregroundStyle(.white)
+                        .font(.system(size: 18, weight: .bold))
+                    Text("유저 정보")
+                        .foregroundStyle(Color(UIColor.lightGray))
+                        .font(.system(size: 16))
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 16)
+        }
+        .background(Color(UIColor.darkGray))
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        
     }
 }
 
