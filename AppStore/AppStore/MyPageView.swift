c5616c3d81b29134d267c24b233dbf73961d5c0d
langpeu <icopy@naver.com>
2025-12-20 00:35:18 +0900

    S2-5. 필요한 라이브러리 설치하기

---
+++ AppStore/AppStore/MyPageView.swift
@@ -0,0 +1,24 @@
+//
+//  MyPageView.swift
+//  AppStore
+//
+//  Created by Langpeu on 12/20/25.
+//
+
+import SwiftUI
+import Alamofire
+import ComposableArchitecture
+
+
+struct MyPageView: View {
+    var body: some View {
+        VStack {
+       
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MyPageView()
+}
